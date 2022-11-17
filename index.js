4a42cb2506b8c5b75a493cee2485f05a7038ac50
neasher <neasherahmed75@gmail.com>
2022-11-18 02:23:49 +0600

    appointment dashboard UI and user info show

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
 
         const appointmentOptionsCollection = client.db('doctorsPortal').collection('appointmentOptions');
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+        const usersCollection = client.db('doctorsPortal').collection('users');
 
         app.get('/appointmentOptions', async (req, res) => {
             const date = req.query.date;
@@ -49,6 +50,14 @@ async function run() {
             res.send(options);
         });
 
+        //get specific bookings for a user
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        });
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             const query = {
@@ -64,7 +73,14 @@ async function run() {
 
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
-        })
+        });
+
+        //users info save in db
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
 
     }
     finally {
